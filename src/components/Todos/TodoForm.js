04dd77339ee28f230f363cbf9d31506501ceed4d
Dmitriy Shafalyuk <dima.shafalyuk@mail.ru>
2023-04-12 14:21:35 +0300

    firstCommit

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import styles from './TodoForm.module.css'
-import Button from '../UI/Button'
+import styles from './TodoForm.module.css';
+import Button from '../UI/Button';
 
 function TodoForm({ addTodo }) {
   const [text, setText] = useState('');
@@ -12,14 +12,16 @@ function TodoForm({ addTodo }) {
 
   return (
     <div className={styles.todoFormContainers}>
-    <form onSubmit={onSubmitHandler}>
-      <input
-        placeholder="Enter new todo"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder="Enter new todo"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button type="submit" title="Submit">
+          Submit
+        </Button>
+      </form>
     </div>
   );
 }
